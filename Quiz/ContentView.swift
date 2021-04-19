029a329a822db1de9085d0e807e8b6c9cebfb17d
heavyrisem <pyo1748@gmail.com>
2021-04-19 10:12:32 +0900

    Initial Commit

---
+++ Quiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quiz
+//
+//  Created by 표인수 on 2021/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by 표인수 on 2021/04/19.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
